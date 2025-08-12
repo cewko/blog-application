f30646c54e31c4469ac5d5345fa9d59ae2490943
cewko <221901489+cewko@users.noreply.github.com>
2025-08-12 14:02:28 +0200

    create seo-friendly urls

--- base/models.py
+++ base/models.py
@@ -17,7 +17,10 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(
+        max_length=255,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -46,4 +49,12 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("base:post_detail", args=[self.id])
+        return reverse(
+            "base:post_detail", 
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
