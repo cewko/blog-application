3fb174c1f59abc95f425aa9acaebb19c0d1c44bd
cewko <221901489+cewko@users.noreply.github.com>
2025-08-12 15:17:35 +0200

    add class based post list view

--- base/views.py
+++ base/views.py
@@ -1,26 +1,22 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import ListView
+from django.http import Http404
+
 from .models import Post
+# from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get("page", 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # if page_number isn't an int get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "base/post/list.html"
 
-    return render(
-        request, 
-        "base/post/list.html", 
-        {"posts": posts}
-    )
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Http404:
+            return redirect(request.path)
 
 
 def post_detail(request, year, month, day, post):
@@ -38,3 +34,23 @@ def post_detail(request, year, month, day, post):
         "base/post/detail.html",
         {"post": post}
     )
+
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get("page", 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # if page_number isn't an int get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+
+#     return render(
+#         request, 
+#         "base/post/list.html", 
+#         {"posts": posts}
+#     )
