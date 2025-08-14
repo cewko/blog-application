3e54ab956e84390be1805284ddeab3a8356b860c
cewko <221901489+cewko@users.noreply.github.com>
2025-08-14 18:38:56 +0200

    add post recommendations by common tags

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from .models import Post
 from .forms import CommentForm, EmailPostForm
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from taggit.models import Tag
-
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -62,14 +62,23 @@ def post_detail(request, year, month, day, post):
     # form for users to comment
     form = CommentForm()
 
-    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count("tags")
+    ).order_by("-same_tags", "-publish")[:4]
+
     return render(
         request,
         "base/post/detail.html",
         {
             "post": post,
             "comments": comments,
-            "form": form
+            "form": form,
+            "similar_posts": similar_posts
         }
     )
 
