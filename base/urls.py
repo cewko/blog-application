eae57ff399c16fd2eeed3ae9aec3a17f959134e9
cewko <221901489+cewko@users.noreply.github.com>
2025-08-15 14:10:04 +0200

    create RSS feed

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = "base"
@@ -14,5 +15,6 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
+    path("feed/", LatestPostsFeed(), name="post_feed")
     # path("", views.PostListView.as_view(), name="post_list"),
 ]
