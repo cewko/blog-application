670f0fb0f45bb767c03fe876fbeed3a6ebd689c2
cewko <221901489+cewko@users.noreply.github.com>
2025-08-15 16:25:54 +0200

    add stemming, ranking and weighing to the search

--- base/views.py
+++ base/views.py
@@ -5,7 +5,11 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
 from .models import Post
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 from .forms import CommentForm, EmailPostForm, SearchForm
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from taggit.models import Tag
@@ -93,10 +97,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector(
+                "title", weight="A"
+            ) + SearchVector("body", weight="B")
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector("title", "body"),
-                ).filter(search=query)
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                ).filter(rank__gte=0.3).order_by("-rank")
             )
 
     return render(
